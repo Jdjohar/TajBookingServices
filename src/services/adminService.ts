a98af98155d244d9a3d1a12455fa3bf83f4750d8
Jdjohar <Jdeep514@gmail.com>
2025-05-23 17:28:21 +0530

    Update AdminLayout with Locations tab

--- src/services/adminService.ts
+++ src/services/adminService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/admin';
+const API_URL = 'https://tajbookingservices.onrender.com/api/admin';
 
 export const fetchDashboardStats = async () => {
   try {
