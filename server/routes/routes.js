a677a0a0a024bbabd40ee65a152163b3a88f8167
Jdjohar <Jdeep514@gmail.com>
2025-05-23 14:44:24 +0530

    Import route handler functions

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import authenticateUser from '../middleware/authentication.js';
 import authorize from '../middleware/authorize.js';
+import { 
+  getRoutes, 
+  getRoute, 
+  createRoute, 
+  updateRoute, 
+  deleteRoute 
+} from '../controllers/routeController.js';
 
 const router = express.Router();
 
