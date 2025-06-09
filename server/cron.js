687e55b681d69d37bf10043077ec542369f84725
Jdjohar <Jdeep514@gmail.com>
2025-06-09 14:22:56 +0530

    update jashans 2255

--- server/cron.js
+++ server/cron.js
@@ -1,7 +1,7 @@
 const cron = require('cron');
 const https = require('https');
 
-const backendUrl = 'https://immaculate.onrender.com/';
+const backendUrl = 'https://tajbookingservices-i051.onrender.com/';
 
 const job = new cron.CronJob('*/14 * * * *', function () {
   // This function will be executed every 14 minutes.
